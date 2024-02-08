d134976575e6340e99e37da5140bc312cf23bf2e
aryafire1 <erthorsen876@gmail.com>
2024-02-08 14:56:13 -0500

    added solution 2

---
+++ Assets/solution2.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class solution2 : MonoBehaviour
+{
+
+    private class die //all dice inheret from this and gain return dice function
+    private class d12 //barbarian
+    private class d10 //fighter, paladin, ranger
+    private class d8 //warlocks, mage, monk, cleric, artificer, druid, bard
+    private class d6 //wizard and sorcerer
+
+    
+}
